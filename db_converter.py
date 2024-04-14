3f2466ac48e23d3f01802317bb43a56ffcce5c86
ChristianTJ <35132082+Nullify11@users.noreply.github.com>
2024-04-14 23:51:46 +0200

    Database Migration
    Added db_converter for extracting and parsing file date and db_migrator for inserting data into database

---
+++ db_converter.py
@@ -0,0 +1,122 @@
+import re
+
+def retrieve_data(file_path, id):
+    data = {'id' : id}
+    file = open(file_path, "r")
+    line = file.readline()
+
+    # Finds the line where the word EPOCH first occurs in file
+    while 'EPOCH' not in line:
+        line = file.readline()
+
+    # Setup regular expression
+    numeric_const_pattern = r'[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+    rx_numbers = re.compile(numeric_const_pattern, re.VERBOSE)
+
+    # Appends relevant data to the data dictionary
+    data.update(create_dictionary(['ec', 'qr', 'tp'], rx_numbers.findall(file.readline())))
+    data.update(create_dictionary(['om', 'w', 'in'], rx_numbers.findall(file.readline())))
+    file.readline()
+    data.update(create_dictionary(['x', 'y', 'z'], rx_numbers.findall(file.readline())))
+    data.update(create_dictionary(['vx', 'vy', 'vz'], rx_numbers.findall(file.readline())))
+    
+    # Read and append the H value
+    file.readline()
+    file.readline()
+    lst = list_convert(file.readline())[0 : 2]
+    data.update(dict_convert(lst))
+
+    # Read until close approaches
+    data.update({'close_approaches' : []})
+    while 'Close-approach results:' not in line:
+        line = file.readline()
+    file.readline()
+    file.readline()
+    file.readline()
+
+    # Append close approaches
+    line = file.readline()
+    while 'A.D.' in line:
+        data['close_approaches'].append(ca_convert(line, id))
+        line = file.readline()
+
+    file.close()
+    return data
+
+
+def ca_convert(line, id):
+    lst = line.split()
+    lst = [lst[1] + ' ' + lst[2] + ' ' + lst[3]] + lst[-3:]
+    lst[2] = float(lst[2])
+    lst[3] = float(lst[3])
+    lst.append(id)
+    return lst
+
+
+def list_convert(line):
+    return line.replace('=', ' ').split()
+
+
+def dict_convert(lst):
+    res_dict = {lst[i].lower() : float(lst[i+1]) for i in range(0, len(lst), 2)}
+    return res_dict
+
+
+def sql_asteroid_fields():
+    return '(id, `ec`, qr, tp, om, w, `in`, x, y, z, vx, vy, vz, h)'
+
+
+def create_dictionary(letters, numbers):
+    res_dict = {letters[i].lower() : float(numbers[i]) for i in range(len(numbers))}
+    return res_dict
+
+
+def sql_asteroid_values(dict):
+    return tuple(dict.values())[:-1]
+
+
+def sql_ca_fields():
+    return '(' + 'date, body, ca_dist, vrel, fk_asteroid_id' + ')'
+
+
+def str_to_float(list):
+    return [float(i) for i in list]
+
+
+def retrieve_data_test(file_path, id):
+    data = {'id' : id}
+    file = open(file_path, "r")
+    line = file.readline()
+
+    numeric_const_pattern = r'[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+    rx_numbers = re.compile(numeric_const_pattern, re.VERBOSE)
+
+    # Finds the line where the word EPOCH first occurs in file
+    while 'EPOCH' not in line:
+        line = file.readline()
+    
+    # Appends relevant data to the data dictionary
+    data.update(create_dictionary(['ec', 'qr', 'tp'], rx_numbers.findall(file.readline())))
+    data.update(create_dictionary(['om', 'w', 'in'], rx_numbers.findall(file.readline())))
+    file.readline()
+    data.update(create_dictionary(['x', 'y', 'z'], rx_numbers.findall(file.readline())))
+    data.update(create_dictionary(['vx', 'vy', 'vz'], rx_numbers.findall(file.readline())))
+
+    # data.update(dict_convert(list_convert(file.readline())))
+    # data.update(dict_convert(list_convert(file.readline())))
+
+    file.close()
+    return data
+
+
+# For testing
+data = retrieve_data('responses_ONE\\response839132.txt', 839132)
+print(data)
+# print(sql_asteroid_fields())
+# print(sql_asteroid_values(data), type(sql_asteroid_values(data)))
+# print(sql_ca_fields())
+# print(tuple(data['close_approaches'][0]))
+
+# ca = data['close_approaches'][0]
+# ca_tuple = tuple(ca)
+# print(ca_tuple, type(ca_tuple))
---
+++ db_migrator.py
@@ -0,0 +1,74 @@
+import mysql.connector
+import db_converter as converter
+import os
+import re
+
+hs_db = mysql.connector.connect(
+  host='localhost',
+  user='root',
+  password='Track_shiftclock1',
+  database='hs_db'
+)
+
+def insert_directory(dir_path, directory_num):
+  initial_index = 1000000 * directory_num
+  directory = os.listdir(dir_path)
+  for i in range(len(directory)):
+    id = initial_index + int(re.findall(r'\d+', directory[i])[0])
+    file_path = dir_path + '\\' + directory[i]
+    insert_item(file_path, id)
+  
+  print(f'Insert for directory {dir_path} succesful!')
+
+
+def insert_item(path, id):
+  data = converter.retrieve_data(path, id)
+  cursor = hs_db.cursor()
+
+  asteroid_exist = is_in_asteroids(id)
+
+  # notify duplicate asteroid entry
+  # if asteroid_exist:
+    # print(f'duplicate asteroid entry found for {id} (duplicate ignored)')
+
+  # Insert asteroid data if it does not exist
+  if not asteroid_exist:
+    sql_insert_asteroid = "INSERT INTO asteroids " + converter.sql_asteroid_fields() + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql_insert_asteroid, converter.sql_asteroid_values(data))
+
+
+  for ca in data['close_approaches']:
+    ca_exists = is_in_ca(ca[0], ca[-1])
+
+    # notify ca already exists
+    # if ca_exists:
+      # print(f'duplicate ca entry found for date: {ca[0]} and foreign key {ca[-1]} (duplicate ignored)')
+
+    if not ca_exists:
+      sql_insert_close_approach = 'INSERT INTO close_approaches ' + converter.sql_ca_fields() + ' VALUES (%s, %s, %s, %s, %s)'
+      cursor.execute(sql_insert_close_approach, tuple(ca))
+  
+  hs_db.commit()
+
+
+def is_in_asteroids(id):
+  cursor = hs_db.cursor()
+  query = 'SELECT id FROM asteroids WHERE id = %s'
+  count = cursor.execute(query, (id,))
+  result = cursor.fetchone()
+  cursor.fetchall()
+  return False if result == None else True
+
+
+def is_in_ca(date:str, fk_asteroid_id):
+  cursor = hs_db.cursor()
+  query = 'SELECT id FROM close_approaches WHERE date = %s AND fk_asteroid_id = %s'
+  count = cursor.execute(query, (date, fk_asteroid_id))
+  result = cursor.fetchone()
+  cursor.fetchall()
+  return False if result == None else True
+
+
+insert_directory('responses_ONE', 0)
+
+# practice_insert('responses_THREE', 3833331)
