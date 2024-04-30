6fc8b3f0d0627b782704f6c1103bfc918f7bfe19
NordiskGhost <162306067+NordiskGhost@users.noreply.github.com>
2024-04-30 07:52:48 +0200

    Update db_extractor.py
    Updated db_extractor.py to now work together with the database.

--- db_extractor.py
+++ db_extractor.py
@@ -1,11 +1,12 @@
 import mysql.connector
 import numpy as np
 import os
+import db_credentials as db_cred
 
 hs_db = mysql.connector.connect(
   host='localhost',
   user='root',
-  password='',
+  password=db_cred.password,
   database='hs_db'
 )
 
@@ -16,30 +17,49 @@ cursor = hs_db.cursor()
 #    result = cursor.fetchall()
 #    return [fk(0) for fk in result]
 
+def count_lines(file_path):
+    with open(f'{file_path}.txt', 'rb') as f:
+        num_lines = sum(1 for _ in f)
+    return num_lines
+
 def extract_elements(min_CA_dist):
-    cursor.execute(f'SELECT primary_key, `ec`, qr, tp, om, w, `in`, h \
+    cursor.execute(f'SELECT asteroids.id, asteroids.`ec`, asteroids.qr, asteroids.tp, asteroids.om, asteroids.w, asteroids.`in`, asteroids.h \
         FROM asteroids \
         INNER JOIN close_approaches \
-        ON asteroids.primary_key = close_approaches.foreign_key \
-        WHERE close_approaches.CA_Dist <= {min_CA_dist}')
+        ON asteroids.id = close_approaches.fk_asteroid_id \
+        WHERE close_approaches.ca_dist <= {min_CA_dist} AND close_approaches.body = "Earth"')
     result = cursor.fetchall()
     return result
 
 #http://www.braeunig.us/space/plntpos.htm
 def plotting_file(min_CA_dist):
     elements_list = extract_elements(min_CA_dist)
-    str_elements_list = [str(element) for element in elements_list]
-    with open(f'plotting_files/plotting{min_CA_dist}.txt','w') as file:
-        for tup in str_elements_list:
-            a = tup[2]/(1-tup[1])
-            file.write(str(tup).strip('()').replace(',','')+f' {a}')
+    print("SQL done")
+    already_inserted = []
+    number_of_CA = 0
+    with open(f'plotting_files/plotting{min_CA_dist}.txt', 'w') as file:
+        for tup in elements_list:
+            number_of_CA += 1
+            try:
+                a = str(float(tup[2])/(1-float(tup[1])))
+            except:
+                print(tup[2],tup[1])
+                a = None
+            if tup[0] not in already_inserted:
+                file.write(str(tup).strip('()').replace(',','')+f' {a}\n')
+                already_inserted.append(tup[0])
+            if len(already_inserted) >= 2:
+                already_inserted.pop(0)
+    number_of_unique_CA = count_lines(f'plotting_files/plotting{min_CA_dist}')
+    with open(f'plotting_files/stats{min_CA_dist}.txt', 'w') as file:
+        file.write(f'Statistics for {min_CA_dist}\nNumber of CA: {number_of_CA}\nNumber of unique CA asteroids: {number_of_unique_CA}')
     print(f'Created plotting file for {min_CA_dist}')
 
 
 def cumulative_freq(min_CA_dist):
-    cursor.execute(f'SELECT foreign_key, vrel \
+    cursor.execute(f'SELECT fk_asteroid_id, vrel \
         FROM close_approaches \
-        WHERE CA_Dist <= {min_CA_dist}')
+        WHERE ca_dist <= {min_CA_dist} AND body = "Earth"')
     result = cursor.fetchall()
     
     #Gravitational constant, the mass of the Earth, and the radius of the Earth
@@ -50,9 +70,10 @@ def cumulative_freq(min_CA_dist):
     prob_dict = {}
     for i in range(len(result)):
         id, relV = result[i]
+        relV = relV*1000 #conversion to m/s
         grav_radius = ((2*gravitational_constant*mass_of_Earth)/(relV**2))*6.6845871226706*10**(-12) # AU    
         cross_section_Earth = np.pi*(r_Earth+grav_radius)**2
-        cross_section_AU = np.pi*0.1**2
+        cross_section_AU = np.pi*min_CA_dist**2
         impact_probability = cross_section_Earth/cross_section_AU
         if id in prob_dict:
             prob_dict[id] = prob_dict[id] + impact_probability
@@ -68,11 +89,9 @@ def cumulative_freq(min_CA_dist):
     total_cumulative_freq = sum(prob_dict.values())
     print(f'Cumulative frequency for {min_CA_dist}:', total_cumulative_freq)
     with open(f'impact_files/cumulative_freq{min_CA_dist}.txt','w') as file:
-        for key in prob_dict:
-            file.write('Total cumulative impact frequency:', total_cumulative_freq, \
-                '\nSlow asteroids and their impact frequency before adjustment:',\
-                str(slow_ast).strip('[]'))
-            file.write(str(key),str(prob_dict[key]))
+        file.write(f'Total cumulative impact frequency: {total_cumulative_freq}\n')
+        file.write(f'Slow asteroids and their impact frequency before adjustment:\n')
+        file.write(str(slow_ast).strip('[]'))
     print(f'Created cumulative frequency file for {min_CA_dist}')
 
 def create_responses_dir(path):
@@ -82,7 +101,7 @@ def create_responses_dir(path):
 def file_creation(min_CA_dist):
     create_responses_dir('plotting_files')
     plotting_file(min_CA_dist)
-    
+
     create_responses_dir('impact_files')
     cumulative_freq(min_CA_dist)
 
@@ -93,5 +112,4 @@ file_creation(0.1)
 file_creation(0.01)
 file_creation(0.001)
 file_creation(0.0001)
-file_creation(0.00001)
 file_creation(r_Earth) #Kun for impacts
